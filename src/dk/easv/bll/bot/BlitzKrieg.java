dd01549782e04bc32685cfb61828d6d11c2ce191
RasWChr <r.west.chr@gmail.com>
2026-03-03 09:08:59 +0100

    Reached all states enlightenment

--- src/dk/easv/bll/bot/BlitzKrieg.java
+++ src/dk/easv/bll/bot/BlitzKrieg.java
@@ -3,22 +3,56 @@ package dk.easv.bll.bot;
 import dk.easv.bll.game.IGameState;
 import dk.easv.bll.move.IMove;
 
-import java.util.List;
-import java.util.Random;
-
 public class BlitzKrieg implements IBot {
     static final String BOTNAME="BlitzKrieg";
-    private Random rand = new Random();
+    static private String[][][] allStates;
+    private final int totalSize = (int) Math.pow(3, 9);
 
+    public static void main(String[] args) {
+        BlitzKrieg blitz = new BlitzKrieg();
+        for (int i = 0; i < 100; i++) {
+            printBoard(allStates[i]);
+        }
+    }
+
+    private static void printBoard(String[][] board){
+        for (String[] tt: board) {
+            System.out.println(tt[0] + tt[1] + tt[2]);
+        }
+        System.out.println("-----");
+    }
+
+    public BlitzKrieg() {
+        allStates = new String[totalSize][3][3];
+        generateAllStates();
+    }
 
     @Override
     public IMove doMove(IGameState state) {
-        List<IMove> moves = state.getField().getAvailableMoves();
-        if (moves.size() > 0) {
-            return moves.get(rand.nextInt(moves.size())); /* get random move from available moves */
-        }
 
         return null;
+    }
+
+    //Generates all board states
+    private void generateAllStates() {
+        //gives different values like x, o or -
+        String[] values = {" - ", " X ", " O "};
+
+        //loops over every single state and writes them down
+        for (int i = 0; i < totalSize; i++) {
+            int num = i;
+            String[][] board = allStates[i];
+
+            for (int j = 0; j < 9; j++) {
+                int valueIndex = num%3;
+                num /= 3;
+                int row = j / 3;
+                int col = j % 3;
+                board[row][col] = values[valueIndex];
+            }
+            allStates[i] = board;
+        }
+
     }
 
     @Override
